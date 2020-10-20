b56b4dd62723bd9098859ae553bbb8611486c4c8
cag2050 <cag2050@163.com>
2020-10-20 15:20:44 +0800

    add: yaml_to_python_object.py

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 1. 安装 pyyaml：`pip install pyyaml`
 2. 执行 read_setting.py，读取yaml文件中的内容
 
+### 使用 virtualenv/venv 和 pip 管理虚拟环境：
+1. 进入项目的根目录，创建虚拟环境：`virtualenv --python=python3 venv`或`python3 -m venv ./venv`（venv不用安装，不过需要python3.3以上）
+2. 进入虚拟环境：`source venv/bin/activate`
+3. 安装依赖：`pip install -r requirements.txt`
+4. 安装新包后，执行这个命令将包名和包版本信息写入 requirements.txt：`pip freeze > requirements.txt`
 
 ### 资料
 资料 | 网址
--- yaml_to_python_object.py
+++ yaml_to_python_object.py
@@ -3,17 +3,19 @@ from yaml import load, Loader
 
 exp_info = [
     {"name": "exp-0", "exp_params_type": "text", "cycle": 0, "cycle_aa": [], "cycle_days": [],
-     "params": "{\"qtt-list-dnn-disliketf\":\" model_name: qtt-biaodan-cvr-sdnn-v6\\n guarantee: qtt-biaodan-cvr-sdnn-rf\"}"
+     "params": "{\"list-dnn-disliketf\":\" model_name: biaodan-cvr-sdnn-v6\\n guarantee: biaodan-cvr-sdnn-rf\"}"
      },
     {"name": "exp-1", "exp_params_type": "text", "cycle": 0, "cycle_aa": [], "cycle_days": [],
-     "params": "{\"qtt-list-ctr-sdnn-merge\":\" model_name: qtt-biaodan-cvr-sdnn-rf\\n guarantee: qtt-adkddcvr-v4-tf\"}"
+     "params": "{\"list-ctr-sdnn-merge\":\" model_name: biaodan-cvr-sdnn-rf\\n guarantee: adkddcvr-v4-tf\"}"
      }
 ]
 
 exp_param_value_sets = set(value for e in exp_info for key, value in json.loads(e['params']).items())
-model_name_set = set()
 print('exp_param_value_sets')
 print(exp_param_value_sets)
+
+model_name_set = set()
+
 # yaml转Python object
 for value in exp_param_value_sets:
     print(load(value, Loader=Loader)['model_name'])
